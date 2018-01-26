0e6c0685fcf043a50acae613f43c4c53a175d81b
Bademiya <miyamirza.esc@gmail.com>
2018-01-26 19:48:59 +0530

    FInal BlogPost app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import PostsIndex from './components/post_index';
 import PostsNew from './components/post_new';
+import PostsShow from './components/post_show';
 
 import reducers from './reducers';
 import {BrowserRouter , Route ,Switch} from 'react-router-dom';
@@ -19,6 +20,7 @@ ReactDOM.render(
    <div>
      <Switch>
      <Route path="/posts/new"  component={PostsNew} />
+     <Route path="/posts/:id"  component={PostsShow}/>
      <Route path="/"  component={PostsIndex} />
      
      </Switch>
